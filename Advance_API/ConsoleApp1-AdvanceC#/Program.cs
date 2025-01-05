7348faa34b3558f75909f257eca165f1d68bd4e5
Rohanshu-33 <banodharohanshu@gmail.com>
2025-01-05 14:56:38 +0530

    advance api started

---
+++ Advance_API/ConsoleApp1-AdvanceC#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ConsoleApp1_AdvanceC_;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Start of Main Program.\n");
+        //Breakpoints.BreakPointsDemo();
+        ConditionalCompilation.ConditionalCompilationDemo();
+        Console.WriteLine("\nEnd of Main Program.");
+    }
+}
